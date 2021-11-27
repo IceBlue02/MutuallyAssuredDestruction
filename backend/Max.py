3b65c7ad252b6261d1d3b3b142c5f8e2d3275c49
a34072mb <maximilian.burnell@student.manchester.ac.uk>
2021-11-27 16:56:28 +0000

    game class set up

--- backend/Max.py
+++ backend/Max.py
@@ -11,19 +11,19 @@ class building(IntEnum):
     silo = 2
 
 class bomb():
-    def __init__(self, name, rarity, description, shape):
+    def __init__(self, id, name, rarity, description, shape):
         self.name = name 
+        self.id = id
         self.rarity = rarity
         self.description = description
         self.shape = shape
 
 
-class cards):
+class cards():
     def __init__(self):
         self.bombs = [None]
 
-
-    def rarity(occurance):
+    def rarity(self, occurance):
         if occurance == "super rare":
             return 1
         if occurance == "rare":
@@ -38,7 +38,7 @@ class cards):
             return 0
 
     def initaliseBombs(self):
-        square = bomb("Square",rarity("average"), "Simple, but effective.", [
+        square = bomb(1,"Square", self.rarity("average"), "Simple, but effective.", [
             [1,1,1,1,0],
             [1,1,1,1,0],
             [1,1,1,1,0],
@@ -47,7 +47,7 @@ class cards):
         ])
         self.bombs.append(square)
 
-        circle = bomb("Circle", rarity("rare"), "The circle of (no) life.", [
+        circle = bomb(2,"Circle", self.rarity("rare"), "The circle of (no) life.", [
             [0,1,1,1,0],
             [1,1,1,1,1],
             [1,1,1,1,1],
@@ -57,7 +57,7 @@ class cards):
         )
         self.bombs.append(circle)
 
-        diamond = bomb("Bomb",rarity("common"), "Diamonds are forever. So is the damage left by this."[
+        diamond = bomb(3,"Bomb",self.rarity("common"), "Diamonds are forever. So is the damage left by this."[
             [0,1,1,1,0,],
             [1,1,1,1,1,],
             [1,1,1,1,1,],
@@ -67,7 +67,7 @@ class cards):
 
         self.bombs.append(diamond)
 
-        target= bomb("Target",rarity("rare"), "You can't miss.",[
+        target= bomb(4,"Target",self.rarity("rare"), "You can't miss.",[
             [1,1,1,1,1],
             [1,0,0,0,1],
             [1,0,1,0,1],
@@ -76,8 +76,7 @@ class cards):
         ])
         self.bombs.append(target)
 
-
-        www_dot_bomb = bomb("wwwDotBomb ", rarity("common"),"...................", [
+        www_dot_bomb = bomb(5,"wwwDotBomb ", self.rarity("common"),"...................", [
             [1,0,1,0,1],
             [0,1,0,1,0],
             [1,0,1,0,1],
@@ -86,7 +85,7 @@ class cards):
         ])
         self.bombs.append(www_dot_bomb)
 
-        X = bomb("X", rarity(""),"It marks the spot. And hits it.", [ 
+        X = bomb(6,"X", self.rarity("common"),"It marks the spot. And hits it.", [ 
             [1,0,0,0,1],
             [0,1,0,1,0],
             [0,0,1,0,0],
@@ -95,7 +94,7 @@ class cards):
         ])
         self.bombs.append(X)
 
-        H0 = bomb("H0",rarity("common"), "Can you add the H0,Bomb to the game?' 'Yeah sure I got you'", [ 
+        H0 = bomb(7,"H0",self.rarity("common"), "Can you add the H0,Bomb to the game?' 'Yeah sure I got you'", [ 
             [1,0,0,0,1],
             [1,0,0,0,1],
             [1,1,1,1,1],
@@ -104,7 +103,7 @@ class cards):
         ])
         self.bombs.append(H0)
 
-        PO = bomb("P0", rarity("average"),"Not sure why you'd make a bomb like this, but there you go.", [ 
+        PO = bomb(8,"P0", self.rarity("average"),"Not sure why you'd make a bomb like this, but there you go.", [ 
             [1,1,1,1,0],
             [1,1,1,1,0],
             [1,1,1,1,0],
@@ -113,7 +112,7 @@ class cards):
         ])
         self.bombs.append(P0)
 
-        cherry = bomb("Cherry", rarity("super rare"), "Yep, it's a pair of Cherries. ", [ 
+        cherry = bomb(9,"Cherry", self.rarity("super rare"), "Yep, it's a pair of Cherries. ", [ 
             [0,0,1,0,0],
             [0,0,1,0,0],
             [0,0,1,0,0],
@@ -122,7 +121,7 @@ class cards):
         ])
         self.bombs.append(cherry)
 
-        e = bomb("e", rarity("rare"), "2.71828182845904523536028747135", [ 
+        e = bomb(10,"e", self.rarity("rare"), "2.71828182845904523536028747135", [ 
             [1,1,1,1,1],
             [1,0,0,0,1],
             [1,1,1,1,1],
@@ -131,7 +130,7 @@ class cards):
         ])
         self.bombs.append(e)
 
-        A0 = bomb("A0",rarity("common"), "I know I'll be A0,O, A0,O0,K." [ 
+        A0 = bomb(11,"A0",self.rarity("common"), "I know I'll be A0,O, A0,O0,K." [ 
             [0,0,1,0,0],
             [0,1,0,1,0],
             [0,1,1,1,0],
@@ -140,7 +139,7 @@ class cards):
         ])
         self.bombs.append(A0)
 
-        England = bomb("ENGLAND",rarity("common"), "RULE BRITANNIA, BRITANNIA RULES THE WAVES", [ 
+        England = bomb(12, "ENGLAND",self.rarity("common"), "RULE BRITANNIA, BRITANNIA RULES THE WAVES", [ 
             [0,0,1,0,0],
             [0,0,1,0,0],
             [1,1,1,1,1],
@@ -150,114 +149,88 @@ class cards):
         self.bombs.append(England)
 
 
-def placeFactories(turn, data):
-    for i in range (0,3):
-        x = data["factories"][i][0]
-        y = data["factories"][i][1]
-        if(board[x][y].state != turn):
-            return False
-        else:
-            board[x][y].building = 1
+class game():
+    def __init__(self):
+        self.deck = [None]
+        self.player = 1
+        self.red_hand =[]
+        self.blue_hand = []
+        self.red_hand_size = board.get_silo_red()
+        self.blue_hand_size = board.get_silo_blue()
+        self.setup = 0
+        self.game_state = 1
 
-def placeSilo(turn, data):
-    for i in range (0,3):
-        x = data["silo"][i][0]
-        y = data["silo"][i][1]
-        if(board[x][y].state != turn):
-            return False
-        else:
-            board[x][y].building = 2   
+    def placeFactories(turn, data):
+        for i in range (0,3):
+            x = data["factories"][i][0]
+            y = data["factories"][i][1]
+            if(board[x][y].state != turn):
+                return False
+            else:
+                board[x][y].building = 1
 
-def get_hand_options(factories):
-    hand_options = [None]
-    for i in range(0, factories):
-        hand_options.append(get_card())
-    return hand_options
 
-def cards():
-    cards = cards()
-    cards.initaliseBombs()
-    
+    def placeSilo(turn, data):
+        for i in range (0,3):
+            x = data["silo"][i][0]
+            y = data["silo"][i][1]
+            if(board[x][y].state != turn):
+                return False
+            else:
+                board[x][y].building = 2   
 
-cards =[None]
-#1 = red
-# -1 = blue
-player = 1
-red_hand_size = board.get_silo_red()
-blue_hand_size = board.get_silo_blue()
-red_hand = []
-blue_hand = []
-gameOver = -1
-set = 0 
-#set factories
-while set == 0:
-    if player == 1:
-        placeFactories(player, data)
-        placeSilo(player, data)
-        player = player* (-1)
-    if player == -1:
-        placeFactories(player, data)
-        placeSilo(player, data)
-        player = player* (-1)
-        set = 1
 
-while gameOver == -1:
-    get_board(player)
-    get_hand_options(get_factory(player))
-    if player ==1:
-        red_hand.append(data["chosen"][0])
-    if player ==-1:
-        blue_hand.append(data["chosen"][0])
+    def get_hand_options(factories):
+        hand_options = [None]
+        for i in range(0, factories):
+            hand_options.append(get_card())
+        return hand_options
 
-    selected_card = data['selected'][0]
-    if player ==1:
-        selected_card = red_hand.pop(selected_card)
-    if player ==-1:
-        selected_card = blue_hand.pop(selected_card)
-    #need to implement placing of bomb 
 
-    get_board(player)
-    player = player*(-1)
+    def get_card():
+        return random.choice(deck)
+
+
+    def deck_builder(self,deck):
+        card_holder = cards()
+        card_holder.initaliseBombs()
+        for i in range(0,card_holder.length()):
+            for _ in range(0,card_holder[i].rarity):
+                deck.apped(card_holder[i])
+   
+    def main(self):
+        deck = [None]
+        game.deck_builder(self.deck)
+        #1 = red
+        # -1 = blue
+        #set factories
+        while self.setup == 0:
+            if self.player == 1:
+                game.placeFactories(self.player, data)
+                game.placeSilo(self.player, data)
+                self.player = self.player* (-1)
+            if self.player == -1:
+                game.placeFactories(self.player, data)
+                game.placeSilo(self.player, data)
+                self.player = self.player* (-1)
+                self.setup = 1
+
+        while self.game_state == 1:
+            board.get_board(player)
+            board.get_hand_options(get_factory(player))
+            if player ==1:
+                self.red_hand.append(data["chosen"][0])
+            if player ==-1:
+                self.blue_hand.append(data["chosen"][0])
+
+            selected_card = data['selected'][0]
+            if player ==1:
+                selected_card = self.red_hand.pop(selected_card)
+            if player ==-1:
+                selected_card = self.blue_hand.pop(selected_card)
+            #need to implement placing of bomb 
+
+            board.get_board(player)
+            player = player*(-1)
 
 #need to write a function to get card.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class AttackCards:
-    def __init__(self,bomb):
-        self.bomb_type = bomb
-    
-    def attacked(x, y):
-        board[x][y].state = 0
-
-    def bombsha(self,x,y):
-        if self.bomb_type == 0:
-            attacked(x, y)
-            #left side
-            attacked((x+1), y)
-            attacked((x+2), y)
-            attacked((x+2), (y+1))
-            attacked((x+2), (y+2))
-            attacked((x+2), (y-1))
-            attacked((x+2), (y-2))
-
-            #right side 
-            attacked((x-1), y)
-            attacked((x-2), y)
-            attacked((x-2), (y+1))
-            attacked((x-2), (y+2))
-            attacked((x-2), (y-1))
-            attacked((x-2), (y-2))
-        
-        if self.bomb_type == 4
