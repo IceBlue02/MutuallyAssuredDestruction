ddb04a160e3ef33a8dc34095da891649735a726f
a34072mb <maximilian.burnell@student.manchester.ac.uk>
2021-11-27 15:40:11 +0000

    started game logic

--- backend/Max.py
+++ backend/Max.py
@@ -16,10 +16,11 @@ class bomb():
         self.shape = shape
 
 
-class game(self):
-    self.bombs = [None]
+class game():
+    def __init__(self):
+        self.bombs = [None]
 
-    def initalBomb(number):
+    def initalBomb(self,number):
         square = bomb("Square", [
             [1,1,1,1,0],
             [1,1,1,1,0],
@@ -129,7 +130,84 @@ class game(self):
             [0,0,1,0,0], 
         ])
         self.bombs.append(England)
-    
+
+
+def placeFactories(turn, data):
+    for i in range (0,3):
+        x = data["factories"][i][0]
+        y = data["factories"][i][1]
+        if(board[x][y].state != turn):
+            return False
+        else:
+            board[x][y].building = 1
+
+def placeSilo(turn, data):
+    for i in range (0,3):
+        x = data["silo"][i][0]
+        y = data["silo"][i][1]
+        if(board[x][y].state != turn):
+            return False
+        else:
+            board[x][y].building = 2   
+
+def get_hand_options(factories):
+    hand_options = [None]
+    for i in range(0, factories):
+        hand_options.append(get_card())
+    return hand_options
+
+#1 = red
+# -1 = blue
+player = 1
+red_hand_size = board.get_silo_red()
+blue_hand_size = board.get_silo_blue()
+red_hand = []
+blue_hand = []
+gameOver = -1
+set = 0 
+#set factories
+while set == 0:
+    if player == 1:
+        placeFactories(player, data)
+        placeSilo(player, data)
+        player = player* (-1)
+    if player == -1:
+        placeFactories(player, data)
+        placeSilo(player, data)
+        player = player* (-1)
+        set = 1
+
+while gameOver == -1:
+    get_board(player)
+    get_hand_options(get_factory(player))
+    if player ==1:
+        red_hand.append(data["chosen"][0])
+    if player ==-1:
+        blue_hand.append(data["chosen"][0])
+
+    selected_card = data['selected'][0]
+    if player ==1:
+        selected_card = red_hand.pop(selected_card)
+    if player ==-1:
+        selected_card = blue_hand.pop(selected_card)
+    #need to implement placing of bomb 
+
+    get_board(player)
+    player = player*(-1)
+
+#need to write a function to get card.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class AttackCards:
