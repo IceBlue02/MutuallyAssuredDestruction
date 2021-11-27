5b8851d771f677917eb3f1eec8c75727b66fd038
a34072mb <maximilian.burnell@student.manchester.ac.uk>
2021-11-27 14:42:57 +0000

    Bombs in Master

--- backend/master.py
+++ backend/master.py
@@ -32,3 +32,121 @@ class Tile:
 
     def set_state(self, state):
         self.state = state
+
+class game():
+    def __init__(self):
+        self.bombs = [None]
+
+    def initalBomb(self,number):
+        square = bomb("Square", [
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+        ])
+        self.bombs.append(square)
+
+        circle = bomb("Circle",[
+            [0,1,1,1,0],
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            [0,1,1,1,0],
+        ]
+        )
+        self.bombs.append(circle)
+
+        diamond = bomb("Bomb", [
+            [0,1,1,1,0,],
+            [1,1,1,1,1,],
+            [1,1,1,1,1,],
+            [1,1,1,1,1,],
+            [0,1,1,1,0,],
+        ])
+
+        self.bombs.append(diamond)
+
+        target= bomb("Target", [
+            [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,0,1,0,1],
+            [1,0,0,0,1],
+            [1,1,1,1,1], 
+        ])
+        self.bombs.append(target)
+
+        www_dot_bomb = bomb("wwwDotBomb ", [
+            [1,0,1,0,1],
+            [0,1,0,1,0],
+            [1,0,1,0,1],
+            [0,1,0,1,0],
+            [1,0,1,0,1], 
+        ])
+        self.bombs.append(www_dot_bomb)
+
+        one = bomb("One", [ 
+            [1,0,0,0,1],
+            [0,1,0,1,0],
+            [0,0,1,0,0],
+            [0,1,0,1,0],
+            [1,0,0,0,1], 
+        ])
+        self.bombs.append(1)
+
+        H0 = bomb("H0", [ 
+            [1,0,0,0,1],
+            [1,0,0,0,1],
+            [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,0,0,0,1],         
+        ])
+        self.bombs.append(H0)
+
+        PO = bomb("P0", [ 
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+            [1,1,1,1,0],
+            [1,0,0,0,0],
+            [1,0,0,0,0],
+        ])
+        self.bombs.append(P0)
+
+        cherry = bomb("Cherry", [ 
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [1,1,0,1,1],
+            [1,1,0,1,1],
+        ])
+        self.bombs.append(cherry)
+
+        e = bomb("e", [ 
+            [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,1,1,1,1],
+            [1,0,0,0,0],
+            [1,1,1,1,1], 
+        ])
+        self.bombs.append(e)
+
+        A0 = bomb("A0", [ 
+            [0,0,1,0,0],
+            [0,1,0,1,0],
+            [0,1,1,1,0],
+            [1,0,0,0,1], 
+            [1,0,0,0,1],           
+        ])
+        self.bombs.append(A0)
+
+        England = bomb("ENGLAND", [ 
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [1,1,1,1,1],
+            [0,0,1,0,0],
+            [0,0,1,0,0], 
+        ])
+        self.bombs.append(England)
+    
+
+
