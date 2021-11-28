7249159d3ce7cde1ca9bfd292ef2771544839de0
Ewan <ewan.iceblue@gmail.com>
2021-11-28 09:12:22 +0000

    Add quick and dirty multiple game support

--- backend/web.py
+++ backend/web.py
@@ -2,10 +2,14 @@ from datetime import MINYEAR
 from flask import Flask, render_template, Response, request, jsonify
 from master import Game
 
-
+ENABLE_MULTI = False
+games = {}
 app = Flask(__name__)
-game = Game()
 
+def get_game(gameid):
+    if not gameid in games.keys():
+        games[gameid] = Game()
+    return games[gameid]
 
 @app.route('/')
 def main():
@@ -14,11 +18,13 @@ def main():
 
 @app.route("/connect", methods=["POST"])
 def on_connect():
+    game = get_game(int(request.json.get("gameid")))
     return jsonify(game.on_game_entry())
 
 
 @app.route("/await_start", methods=["GET"])
 def await_start():
+    game = get_game(int(request.json.get("gameid")))
     def waitforstart():
         while True:
             game.await_game_start()
@@ -29,11 +35,13 @@ def await_start():
 
 @app.route("/get_cards", methods=["GET"])
 def get_bombs():
+    game = get_game(int(request.json.get("gameid")))
     return jsonify(game.get_cards())
 
 
 @app.route("/place_starting_board", methods=["POST"])
 def place_starting_board():
+    game = get_game(int(request.json.get("gameid")))
     player = request.json.get("player")
     factories = request.json.get("factories")
     silos = request.json.get("silos")
@@ -42,6 +50,7 @@ def place_starting_board():
 
 @app.route("/await_turn", methods=["GET"])
 def await_turn():
+    game = get_game(int(request.json.get("gameid")))
     def waitforturn():
         while True:
             playercode = game.await_turn_change()
@@ -51,12 +60,14 @@ def await_turn():
 
 @app.route("/get_game_state", methods=["POST"])
 def get_game_state():
+    game = get_game(int(request.json.get("gameid")))
     player = request.json.get("player")
     return jsonify(game.get_game_state(int(player)))
 
 
 @app.route("/get_hand_options", methods=["POST"])
 def get_hand_options():
+    game = get_game(int(request.json.get("gameid")))
     player = request.json.get("player")
     options = game.get_hand_options(player)
     return jsonify(options)
@@ -64,6 +75,7 @@ def get_hand_options():
 
 @app.route("/choose_card", methods=["POST"])
 def choose_card():
+    game = get_game(int(request.json.get("gameid")))
     player = request.json.get("player")
     cardid = request.json.get("cardId")
     valid = game.choose_card(player, cardid)
@@ -72,11 +84,20 @@ def choose_card():
 
 @app.route("/place_bomb", methods=["POST"])
 def place_bomb():
+    game = get_game(int(request.json.get("gameid")))
     player = int(request.json.get("player"))
     coords = request.json.get("coords") # 2 length list
     bombid = request.json.get("bombId")
     success = game.deploy_bomb(player, coords, bombid)
     return jsonify({"outcome": success})
+
+@app.route("/on_disconnect", methods=["POST"])
+def on_disconnect():
+    if int(request.json.get("gameid")) in games.keys():
+        del games[int(request.json.get("gameid"))]
+    
+
+
 
 
 if __name__ == '__main__':
